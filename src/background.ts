25ffdbb7adc51ffb1b16ed7ee074d9ecece5ea71
Pieter Vanderpol <pjvanderpol@gmail.com>
2022-02-20 12:31:14 -0800

    temp backup

--- src/background.ts
+++ src/background.ts
@@ -1,25 +1,30 @@
 import { resolve } from "path/posix";
 import { isSiteBlocked } from "./helpers";
-import { AppStorage } from "./types";
+import { AppStorage, APP_STORAGE_KEYS } from "./types";
 
 let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ sitesText: '' });
+  chrome.storage.sync.set({ sitesText: '', boopCounts: {} });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
 
 
 const checkBlocked = async (details: chrome.webNavigation.WebNavigationParentedCallbackDetails | chrome.webNavigation.WebNavigationFramedCallbackDetails) => {
   const urlTabsPromise = chrome.tabs.query({url: details.url})
-  const {sitesText} =  await new Promise<AppStorage>((resolve) => {
-    chrome.storage.local.get("sitesText", (res) => resolve(res as AppStorage));
+  const {sitesText, boopCounts} =  await new Promise<AppStorage>((resolve) => {
+    chrome.storage.local.get(APP_STORAGE_KEYS, (res) => resolve(res as AppStorage));
   });
   if (isSiteBlocked(sitesText, details.url)) {
     const tabs = await urlTabsPromise;
     tabs.forEach((tab) => {
       chrome.tabs.update(tab.id as number, {url: chrome.runtime.getURL("blocked.html")});
-    })
+    });
+    if (!!boopCounts[details.url]) {
+      boopCounts[details.url]++
+    } else {
+      boopCounts[details.url] = 1;
+    }
   }
 }
 
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
 export interface AppStorage {
   sitesText: string;
+  boopCounts: { [key: string]: number };
 }
+
+export const APP_STORAGE_KEYS = ["sitesText", "boopCounts"];
